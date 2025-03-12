7a7f659449cd0226ab9d4ac24939a31008206a43
Abayev <240918@astanait.edu.kz>
2025-03-12 22:01:57 +0500

    git init

---
+++ src/main/java/org/met/shop_met/ShopMetApplication.java
@@ -0,0 +1,13 @@
+package org.met.shop_met;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/met/shop_met/ShopMetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.met.shop_met;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
